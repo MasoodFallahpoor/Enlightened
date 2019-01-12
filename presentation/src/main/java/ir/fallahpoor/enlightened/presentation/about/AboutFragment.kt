0aa6de23f667b1f1ad13171aa1aa12ac4447eb90
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2019-01-12 11:18:15 +0330

    Add app version to about Fragment

--- presentation/src/main/java/ir/fallahpoor/enlightened/presentation/about/AboutFragment.kt
+++ presentation/src/main/java/ir/fallahpoor/enlightened/presentation/about/AboutFragment.kt
@@ -5,14 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ir.fallahpoor.enlightened.BuildConfig
 import ir.fallahpoor.enlightened.R
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =
-        inflater.inflate(R.layout.fragment_about, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_about, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        appVersionTextView.text =
+                getString(R.string.app_version_formatted, BuildConfig.VERSION_NAME)
+    }
 
 }
